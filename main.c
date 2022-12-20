089c0b1daa03c95926bafb9564fc7b3404062d4a
NoahMic <doyun5854@gmail.com>
2022-12-20 11:21:27 +0900

    연산자 별 결과 추가

--- main.c
+++ main.c
@@ -2,10 +2,29 @@
 #include <stdio.h>
 #include <time.h>
 
+char oper[] = {'+', '-', '*', '/'};
+
 int main() {
     srand(time(NULL));
     for (int i = 1; i <= 10; i++) {
         int a = rand() % 21, b = rand() % 21;
-        printf("[%d/10] %d + %d = \n", i , a, b);
+        int op = rand() % 4;
+        printf("[%d/10] %d %c %d = ", i , oper[op], a, b);
+        int ans;
+        switch (op) {
+            case 0:
+                ans = a + b;
+                break;
+            case 1:
+                ans = a - b;
+                break;
+            case 2:
+                ans = a * b;
+                break;
+            case 3:
+                ans = a / b;
+                break;
+        }
+        printf("%d\n", ans);
     }
 }
